a5d5f680772754586932c619a835ca84d5dcfade
Andy Stutzman <astutzman@drexel.edu>
2018-03-05 12:08:00 -0500

    updating relative url links

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,8 +8,8 @@
         <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!--<link rel="stylesheet" href="/css/bootstrap.min.css" media="screen">-->
-    <link rel="stylesheet" href="/css/app.css">
-      <link rel="stylesheet" href="/css/custom.css">
+    <link rel="stylesheet" href="css/app.css">
+      <link rel="stylesheet" href="css/custom.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" />
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
